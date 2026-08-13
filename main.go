26c1be3e79e7e4fab53bf322739e574f1aee3518
Lilrosco <rossey.charleston@gmail.com>
2026-08-13 00:10:00 -0400

    Created a REPL for the project

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			input := scanner.Text()
+
+			if len(input) == 0 {
+				continue
+			}
+
+			cleanInput := cleanInput(input)
+			fmt.Printf("Your command was: %s\n", cleanInput[0])
+		}
+	}
 }
